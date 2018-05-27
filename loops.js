7f47ba19821715a0e2d29d3d7e2fc0cf01520018
Christina L <clingc7@gmail.com>
2018-05-27 14:58:05 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -27,8 +27,6 @@ function doWhileLoop(array) {
 }
   do {
     array.pop;
-  } while (array.length > 0, maybeTrue()) {
-   return Math.random() >= 0.5
-  }
+  } while (array.length > 0 && maybeTrue()) 
   return array 
 }
